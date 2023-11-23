4abfa26daf7f879959dc71a613045e448461ff80
eglisonsouza <79208618+eglisonsouza@users.noreply.github.com>
2023-11-23 15:04:52 -0300

    feat: criado exemplo dependency inversion

---
+++ DemoSolid.DIP/PersonService.cs
@@ -0,0 +1,30 @@
+﻿namespace DemoSolid.DIP
+{
+    public class PersonService : IPersonService
+    {
+        private readonly IPersonRepository _personRepository;
+        private readonly IServiceBus _serviceBus;
+        private readonly IErpSyncService _erpSyncService;
+
+        public PersonService(
+            IPersonRepository personRepository,
+            IServiceBus serviceBus,
+            IErpSyncService erpSyncService)
+        {
+            _personRepository = personRepository;
+            _serviceBus = serviceBus;
+            _erpSyncService = erpSyncService;
+        }
+
+        public void Add(PersonDto dto)
+        {
+            var person = new Person(dto.Name, dto.Document, dto.BirthDate);
+
+            _personRepository.Add(person);
+
+            _erpSyncService.SyncPerson(person);
+
+            _serviceBus.Publish("name_queue", person);
+        }
+    }
+}
---
+++ DemoSolid.DIP/ServiceBus.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoSolid.DIP
+{
+    public class ServiceBus : IServiceBus
+    {
+        public void Publish(string queue, Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

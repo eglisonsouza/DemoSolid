159871cfc076e36e401fd193066828e4885636d5
eglisonsouza <79208618+eglisonsouza@users.noreply.github.com>
2023-11-23 08:21:09 -0300

    feat: exemplo open-close

---
+++ DemoSolid.OCP/LoyaltyPointsMethod.cs
@@ -0,0 +1,14 @@
+﻿namespace DemoSolid.OCP
+{
+    public class LoyaltyPointsMethod : IOrderPaymentMethod
+    {
+        public LoyaltyPointsMethod(OrderPaymentInfo orderPaymentInfo)
+        {
+        }        
+
+        public void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DemoSolid.OCP/OrderPaymentInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoSolid.OCP
+{
+    public class OrderPaymentInfo
+    {
+        public Guid OrderId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public PaymentType Type { get; set; }
+    }
+
+    public enum PaymentType
+    {
+        Credit = 0,
+        Debit = 1,
+        LoyaltyPoints = 2
+    }
+}
